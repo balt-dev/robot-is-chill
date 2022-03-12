7291a91e186cfa65c19da97b887edd235955dd39
CenTdemeern1 <timo.herngreen@gmail.com>
2022-03-12 14:34:44 +0100

    Convert to tabs (WHY DID THIS TURN INTO SPACES)

--- src/cogs/render.py
+++ src/cogs/render.py
@@ -917,7 +917,7 @@ class Renderer:
 				for y in range(len(im)):
 					for x in range(len(im[0])):
 						if all([x == 0 for x in im[y,x,:3]]) and im[y,x,3] == 255:
-                            brightnessvalue=round(value*255)
+							brightnessvalue=round(value*255)
 							im[y,x,:] = np.array([brightnessvalue,brightnessvalue,brightnessvalue,255])  #somehow this doesn't fuck up anywhere
 				sprite = Image.fromarray(np.array(im))
 			elif name == 'colselect' and value != None:
