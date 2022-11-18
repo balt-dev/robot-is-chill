185bc168a15b534e332faca7045691a4fe4a96c8
Balt <heptor42@gmail.com>
2022-11-18 15:28:59 -0600

    Update db.py

--- src/db.py
+++ src/db.py
@@ -72,14 +72,22 @@ class Database:
 					tags TEXT NOT NULL DEFAULT "",
 					UNIQUE(name, version)
 				);
+				'''
+            )
+            await cur.execute(
+                '''
                 CREATE TABLE IF NOT EXISTS ServerActivity (
 					id INTEGER NOT NULL,
 					timestamp INTEGER NOT NULL
 				);
+				'''
+            )
+            await cur.execute(
+                '''
                 CREATE TABLE IF NOT EXISTS blacklistedusers (
 					id INTEGER NOT NULL
 				);
-				'''
+                '''
             )
             # We create different tables for levelpacks and custom levels.
             # While both share some fields, there are mutually exclusive
