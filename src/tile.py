7eee0c7c184b29975554e6d254f0a239772d098b
Balt <heptor42@gmail.com>
2023-02-10 06:46:36 -0600

    Cleaning up, text manip

--- src/tile.py
+++ src/tile.py
@@ -1,16 +1,12 @@
 from __future__ import annotations
 
-import traceback
-import typing
 from dataclasses import dataclass, field
 
-from src.constants import BABA_WORLD
-from typing import TYPE_CHECKING, Literal, TypedDict
+from typing import Literal
 from PIL import Image
 import re
 import numpy as np
 from . import errors, constants
-from .cogs.generator import CharacterGenerator
 from .cogs.variants import parse_signature
 from .db import TileData
 from .types import Variant, Context
@@ -20,12 +16,16 @@ from .types import Variant, Context
 class TileSkeleton:
     """A tile that hasn't been assigned a sprite yet."""
     name: str = "<empty tile>"
+    raw_string: str = ""
     variants: dict[str: Variant, str: Variant, str: Variant] = field(
         default_factory=lambda: {"sprite": [], "tile": [], "post": []})
+    palette: str = "default"
     empty: bool = True
 
     @classmethod
-    def parse(cls, possible_variants, string: str, rule: bool = True, macros: dict[str, str] = {}):
+    def parse(cls, possible_variants, string: str, rule: bool = True, macros=None, palette: str = "default"):
+        if macros is None:
+            macros = {}
         out = cls()
         if string == "-":
             return out
@@ -35,7 +35,9 @@ class TileSkeleton:
             else:
                 string = "text_" + string
         out.empty = False
-        raw_variants = re.split(r";|:", string)
+        out.raw_string = string
+        out.palette = palette
+        raw_variants = re.split(r"[;:]", string)
         out.name = raw_variants.pop(0)
         macro_count = 0
         for raw_variant in raw_variants:
@@ -64,7 +66,7 @@ def is_adjacent(pos, tile, grid, tile_borders=False) -> bool:
     return grid[w, z, y, x].name in joining_tiles
 
 
-def get_bitfield(*arr: typing.Iterable[bool]):
+def get_bitfield(*arr: bool):
     return sum(b << a for a, b in enumerate(list(arr)[::-1]))
 
 
@@ -74,7 +76,7 @@ def handle_tiling(tile: Tile, grid, pos, tile_borders=False):
     adj_u = is_adjacent((w, x, y - 1, z), tile, grid, tile_borders)
     adj_l = is_adjacent((w, x - 1, y, z), tile, grid, tile_borders)
     adj_d = is_adjacent((w, x, y + 1, z), tile, grid, tile_borders)
-    fallback = constants.TILING_VARIANTS[get_bitfield(adj_r, adj_u, adj_l, adj_d, 0, 0, 0, 0)]
+    fallback = constants.TILING_VARIANTS[get_bitfield(adj_r, adj_u, adj_l, adj_d, False, False, False, False)]
     # Variant with diagonal tiles as well, not guaranteed to exist
     # The renderer falls back to the simple variant if it doesn't
     adj_ru = adj_r and adj_u and is_adjacent(
@@ -85,8 +87,10 @@ def handle_tiling(tile: Tile, grid, pos, tile_borders=False):
         (w, x - 1, y + 1, z), tile, grid, tile_borders)
     adj_rd = adj_d and adj_r and is_adjacent(
         (w, x + 1, y + 1, z), tile, grid, tile_borders)
-    tile.frame = constants.TILING_VARIANTS.get(get_bitfield(adj_r,adj_u,adj_l,adj_d,adj_ru,adj_lu,adj_ld,adj_rd), fallback)
+    tile.frame = constants.TILING_VARIANTS.get(get_bitfield(adj_r, adj_u, adj_l, adj_d, adj_ru, adj_lu, adj_ld, adj_rd),
+                                               fallback)
     tile.fallback_frame = fallback
+
 
 @dataclass
 class Tile:
@@ -97,12 +101,12 @@ class Tile:
     surrounding: int = 0b00000000  # RULDEQZC
     frame: int = 0
     fallback_frame: int = 0
-    color: tuple[int, int] | tuple[int, int, int] = (0, 3)  # x, y / r, g, b
-    default_color: tuple[int, int] = (0, 3)
+    custom_color: bool = False
+    color: tuple[int, int] = (0, 3)
     empty: bool = True
-    blending: Literal["NORMAL", "ADD", "SUB", "MULT", "CUT", "MASK"] = "NORMAL"
+    blending: Literal["NORMAL", "ADD", "SUB", "MULT", "CUT", "MASK", "XOR"] = "NORMAL"
     custom: bool = False
-    custom_style: Literal["noun", "property", "letter"] = "noun"
+    style: Literal["noun", "property", "letter"] = "noun"
     palette: str = None
     overlay: str | None = None
     hue: float = 1.0
@@ -112,7 +116,18 @@ class Tile:
     displacement: tuple[int, int] = (0, 0)
     palette_snapping: bool = False
     normalize_gamma: bool = False
-    variants: dict[str: Variant, str: Variant, str: Variant] = None
+    variants: dict[str, list] = None
+    altered_frame: bool = False
+
+    def __hash__(self):
+        return hash((self.name, self.sprite if type(self.sprite) is tuple else 0, self.frame, self.fallback_frame,
+                     self.color, self.empty, self.blending, self.custom,
+                     self.style, self.palette, self.overlay, self.hue,
+                     self.gamma, self.saturation, self.filterimage, self.displacement,
+                     self.palette_snapping, self.normalize_gamma,
+                     hash(tuple(self.variants["sprite"])),
+                     hash(tuple(self.variants["tile"])),
+                     hash(tuple(self.variants["post"])), self.custom_color, self.palette))
 
     @classmethod
     def prepare(cls, tile: TileSkeleton, tile_data_cache: dict[str, TileData], grid,
@@ -122,20 +137,22 @@ class Tile:
         name = tile.name
         try:
             metadata = tile_data_cache[name]
+            style = constants.TEXT_TYPES[metadata.text_type]
             value = cls(name=name, sprite=(metadata.source, metadata.sprite), tiling=metadata.tiling,
-                        color=metadata.active_color, default_color=metadata.active_color,
-                        variants=tile.variants, empty=False)
+                        color=metadata.active_color, variants=tile.variants, empty=False, style=style,
+                        palette=tile.palette)
             if metadata.tiling == constants.TILING_TILE:
                 handle_tiling(value, grid, position, tile_borders=tile_borders)
         except KeyError:
             if name[:5] == "text_":
-                value = cls(name=name, tiling=constants.TILING_NONE, variants=tile.variants, empty=False, custom=True)
+                value = cls(name=name, tiling=constants.TILING_NONE, variants=tile.variants, empty=False, custom=True,
+                            palette=tile.palette)
             elif name[:5] == "char_" and ctx is not None:  # allow external calling for potential future things?
                 seed = int(name[5:]) if re.match(r'-?\d+', name[5:]) else name[5:]
-                character = ctx.bot.generator.generate(True, seed = seed)
+                character = ctx.bot.generator.generate(True, seed=seed)
                 color = character[1]["color"][1]
                 value = cls(name=name, tiling=constants.TILING_CHAR, variants=tile.variants, empty=False, custom=True,
-                            sprite=character[0], color=color, default_color=color)
+                            sprite=character[0], color=color, palette=tile.palette)
             else:
                 raise errors.TileNotFound(name)
         for variant in value.variants["tile"]:
@@ -144,16 +161,6 @@ class Tile:
                 value.frame = constants.TILING_VARIANTS[value.surrounding]
                 value.fallback_frame = constants.TILING_VARIANTS[value.surrounding & 0b11110000]
         return value
-
-    def __hash__(self):
-        return hash((self.name, self.sprite if type(self.sprite) is tuple else 0, self.frame, self.fallback_frame,
-                     self.color, self.empty, self.blending, self.custom,
-                     self.custom_style, self.palette, self.overlay, self.hue,
-                     self.gamma, self.saturation, self.filterimage, self.displacement,
-                     self.palette_snapping, self.normalize_gamma,
-                     hash(tuple(self.variants["sprite"])),
-                     hash(tuple(self.variants["tile"])),
-                     hash(tuple(self.variants["post"]))))
 
 
 @dataclass
