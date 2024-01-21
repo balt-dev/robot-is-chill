725d46ac87a9bd549ca1d74977227551212ae219
baltdev <heptor42@gmail.com>
2024-01-21 10:40:26 -0600

    fixes of fixes

--- src/cogs/variants.py
+++ src/cogs/variants.py
@@ -564,9 +564,9 @@ If [0;36mextrapolate[0m is on, then colors outside the gradient will be extrap
         """Applies a meta filter to an image."""
         if level is None: level = 1
         if size is None: size = 1
-        assert size < 0, f"Size of {size} too small!"
-        assert size > MAX_META_SIZE, f"Size of {size} too small!"
-        assert abs(level) <= constants.MAX_META_DEPTH, f"Meta depth of {level} too large!"
+        assert size > 0, f"The given meta size of {size} is too small!"
+        assert size <= constants.MAX_META_SIZE, f"The given meta size of {size} is too large! Try something lower than `{constants.MAX_META_SIZE}`."
+        assert abs(level) <= constants.MAX_META_DEPTH, f"Meta depth of {level} too large! Try something lower than `{constants.MAX_META_DEPTH}`."
         # Not padding at negative values is intentional
         padding = max(level*size, 0)
         orig = np.pad(sprite, ((padding, padding), (padding, padding), (0, 0)))
